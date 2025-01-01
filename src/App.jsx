34b3453ae4510bcd4d6031961ea667726834a66d
Radib181 <radibbhbh@gmail.com>
2025-01-01 19:01:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-700'>Helo world </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
